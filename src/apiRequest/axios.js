eb483205fcd82e78203fa04e7f6688c4a974e91d
lohrenzo <lorenzofblack@gmail.com>
2024-05-20 22:02:37 +0100

    VITE_API_URL

--- src/apiRequest/axios.js
+++ src/apiRequest/axios.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const axiosPrivate = axios.create({
   // Attach interceptors to the axios
-  // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
